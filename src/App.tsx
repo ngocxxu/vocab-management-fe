e7db6300cfda65f26a7560c78c5a9c4cb1f01b35
ngocxxu <ngocquach43@gmail.com>
2023-12-24 18:05:38 +0700

    config firebase, github action

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,18 @@ import VocabTrainer from './pages/vocab-trainer';
 import History from './pages/history';
 import { MantineProvider, createTheme } from '@mantine/core';
 import '@mantine/core/styles.css';
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAhopedDbZte4fXSGgg5onc9iOxiqYBZN4',
+  authDomain: 'vocab-management.firebaseapp.com',
+  projectId: 'vocab-management',
+  storageBucket: 'vocab-management.appspot.com',
+  messagingSenderId: '314055413964',
+  appId: '1:314055413964:web:ceb855a96a28cf1403f269',
+};
+
+initializeApp(firebaseConfig);
 
 const theme = createTheme({
   /** Put your mantine theme override here */
