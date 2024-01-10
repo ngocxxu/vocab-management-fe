7d11b71c189a1bec388859c3e460ee836a7b5e2f
ngocxxu <ngocquach43@gmail.com>
2024-01-11 00:47:02 +0700

    edit pipeline

--- src/components/table/index.tsx
+++ src/components/table/index.tsx
@@ -6,8 +6,8 @@ type TTable<T> = {
 
 const Table = <T,>({ table }: TTable<T>) => {
   return (
-    <table className='w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 table-fixed'>
-      <thead className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400'>
+    <table className='w-full text-sm text-left rtl:text-right text-gray-500 table-fixed'>
+      <thead className='text-xs text-gray-700 uppercase bg-gray-50'>
         {table.getHeaderGroups().map((headerGroup) => (
           <tr key={headerGroup.id}>
             {headerGroup.headers.map((header) => (
@@ -25,10 +25,7 @@ const Table = <T,>({ table }: TTable<T>) => {
       </thead>
       <tbody>
         {table.getRowModel().rows.map((row) => (
-          <tr
-            className='bg-white dark:bg-gray-800 dark:border-gray-700 border-b'
-            key={row.id}
-          >
+          <tr className='bg-white border-b' key={row.id}>
             {row.getVisibleCells().map((cell) => (
               <td className='px-6 py-4' key={cell.id}>
                 {flexRender(cell.column.columnDef.cell, cell.getContext())}
