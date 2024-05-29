095d26c70b819df8eee3774e9b5aca1e0f6bbe43
bono43 <bono43@nexondv.com>
2024-05-29 15:45:03 +0700

    change progres bar lib

--- src/pages/vocab-trainer/components/circleProgress/index.tsx
+++ src/pages/vocab-trainer/components/circleProgress/index.tsx
@@ -1,16 +1,24 @@
-import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
-
-const percentage = 66;
+import ProgressBar from 'react-customizable-progressbar';
 
 export const CircleProgress = () => {
   return (
-    <div style={{ width: 200, height: 200 }}>
-       <CircularProgressbar
-        value={percentage}
-        text={`${percentage}%`}
-        strokeWidth={15}
-      />
-    </div>
+    <ProgressBar
+      progress={60}
+      radius={120}
+      strokeWidth={50}
+      strokeColor='#14B8A6'
+      trackStrokeWidth={50}
+      trackStrokeColor='#E4E6EF'
+      pointerRadius={18}
+      pointerStrokeWidth={0}
+    >
+      <div className='flex justify-center items-center absolute top-0 w-full h-full'>
+        <div>
+          <p className='text-3xl font-bold'>{60}%</p>
+          <p className='text-customBlack1 font-normal mt-1 text-center'>5/10</p>
+        </div>
+      </div>
+    </ProgressBar>
   );
 };
--- src/pages/vocab-trainer/components/resultStatistic/index.tsx
+++ src/pages/vocab-trainer/components/resultStatistic/index.tsx
@@ -1,18 +1,22 @@
-import { IconSquareCheck } from '@tabler/icons-react';
+import { IconClock, IconSlash, IconSquareCheck } from '@tabler/icons-react';
 import { CircleProgress } from '../circleProgress';
 
 export const ResultStatistic = () => {
   return (
     <div className='container my-10 grid grid-cols-9 gap-4'>
-      <div className='col-span-4 bg-white rounded-md p-6 font-semibold shadow-md border-t'>
-        <p>Result</p>
+      <div className='col-span-4 bg-white rounded-md p-6 pb-0 font-semibold shadow-md border-t'>
+        <p className='text-lg font-bold mb-1'>Result</p>
         <div className='flex justify-between items-start'>
           <div>
-            <div className='flex gap-2'>
-              <IconSquareCheck />
+            <div className='flex gap-2 text-2xl text-customGreen2 mt-4'>
+              <div className='mt-1'>
+                <IconSquareCheck />
+              </div>
               <div>
                 <p>Test passed</p>
-                <p>Thank you for taking the test.</p>
+                <p className='text-sm text-customGray font-normal mt-3'>
+                  Thank you for taking the test.
+                </p>
               </div>
             </div>
           </div>
@@ -21,7 +25,36 @@ export const ResultStatistic = () => {
         </div>
       </div>
       <div className='col-span-5 bg-white rounded-md p-6 font-semibold shadow-md border-t'>
-        2
+        <p className='text-lg font-bold mb-1'>Timer</p>
+        <div className='flex gap-2 text-2xl mt-4'>
+          <div className='mt-1'>
+            <IconClock />
+          </div>
+          <div className='w-full'>
+            <p>Total time</p>
+            <div className='flex items-center mt-6 mb-4'>
+              <p>00:12:04</p>
+              <IconSlash className='text-customGray mx-4' />
+              <p className='text-customGray'>00:30:00</p>
+            </div>
+
+            <div className='relative w-full'>
+              <span
+                className='absolute bottom-1 z-10 -translate-x-5 w-4 h-4 bg-white rounded-full'
+                style={{ left: '50%' }}
+              />
+              <div className='relative flex w-full h-6 overflow-hidden rounded-3xl bg-gray-100'>
+                <div className='w-full  bg-gray-100 rounded-3xl h-6 '>
+                  <div
+                    role='progressbar'
+                    className='bg-customBlack1 h-6 rounded-3xl'
+                    style={{ width: '50%' }}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <div className='col-span-9 bg-white rounded-md p-6 font-semibold shadow-md border-t'>
         3
