ab790a1ad3d44dffc12b2ff694f18971749c3ecc
ngocxxu <ngocquach43@gmail.com>
2024-07-19 00:20:09 +0700

    feat: adjust sidebar menu

--- src/pages/layout/components/sidebar/index.tsx
+++ src/pages/layout/components/sidebar/index.tsx
@@ -1,66 +1,14 @@
-import IconBook from '@/assets/svg/IconBook'
 import IconBurger from '@/assets/svg/IconBurger'
-import IconChatGPT from '@/assets/svg/IconChatGPT'
-import IconExam from '@/assets/svg/IconExam'
-import IconGlobal from '@/assets/svg/IconGlobal'
-import IconHome from '@/assets/svg/IconHome'
 import IconLogo from '@/assets/svg/IconLogo'
-import IconPhone from '@/assets/svg/IconPhone'
-import IconSetting from '@/assets/svg/IconSetting'
+import IconLogout from '@/assets/svg/IconLogout'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Separator } from '@/components/ui/separator'
 import { cn } from '@/lib/utils'
 import { Breakpoint } from '@/utils/enum'
 import { ComponentType, SVGProps, useEffect, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import { useWindowSize } from 'react-use'
-
-const menuList = [
-  {
-    title: 'MENU',
-    list: [
-      {
-        icon: IconHome,
-        label: 'Dashboard',
-        link: '/'
-      },
-      {
-        icon: IconBook,
-        label: 'Vocab List',
-        link: '/vocab'
-      },
-      {
-        icon: IconExam,
-        label: 'Vocab Trainer',
-        link: '/vocab-trainer'
-      },
-      {
-        icon: IconChatGPT,
-        label: 'Chatting with AI',
-        link: '/chat-gpt'
-      },
-      {
-        icon: IconGlobal,
-        label: 'Community',
-        link: '/community'
-      }
-    ]
-  },
-  {
-    title: 'SUPPORT',
-    list: [
-      {
-        icon: IconSetting,
-        label: 'Settings',
-        link: '/settings'
-      },
-      {
-        icon: IconPhone,
-        label: 'Help & Support',
-        link: '/help-support'
-      }
-    ]
-  }
-]
+import { menuList } from '../../constants'
 
 type TIcon = {
   icon: ComponentType<SVGProps<SVGSVGElement>>
@@ -88,12 +36,15 @@ export const Sidebar = () => {
   return (
     <div
       className={cn(
-        'flex-none rounded-xl border px-4 py-6 font-medium shadow-md transition-all delay-300 ease-in-out',
+        'flex-none rounded-xl border px-4 py-6 font-medium shadow-md transition-all duration-500 ease-in-out',
         isCollapseMenu ? 'w-vertical-menu-sm' : 'w-vertical-menu'
       )}
     >
       <div
-        className={cn('flex flex-col gap-6', isCollapseMenu && 'items-center')}
+        className={cn(
+          'flex h-full flex-col gap-6',
+          isCollapseMenu && 'items-center'
+        )}
       >
         <div className="flex items-center justify-between">
           {!isCollapseMenu && <IconLogo />}
@@ -117,7 +68,7 @@ export const Sidebar = () => {
                   to={link}
                   className={({ isActive }) =>
                     cn(
-                      'relative flex items-center gap-2 rounded-lg px-4 py-2.5 transition-colors',
+                      'relative flex items-center gap-2 rounded-lg px-4 py-2.5 transition-colors ease-in-out',
                       isActive && !isCollapseMenu ?
                         'bg-gradient-to-r from-blue-50 to-white font-bold text-primary-vc-500'
                       : 'text-gray-vc-600 hover:bg-gray-vc-100'
@@ -130,8 +81,14 @@ export const Sidebar = () => {
                       {isActive && (
                         <div className="absolute bottom-0 left-0 top-0 w-1 rounded-r-lg bg-primary-vc-500" />
                       )}
-                      <Icon icon={icon} isActive={isActive} />
-                      {!isCollapseMenu && label}
+                      <div className="transition-opacity duration-500 ease-in-out">
+                        <Icon icon={icon} isActive={isActive} />
+                      </div>
+                      {!isCollapseMenu && (
+                        <span className="whitespace-nowrap transition-opacity duration-500 ease-in">
+                          {label}
+                        </span>
+                      )}
                     </>
                   )}
                 </NavLink>
@@ -139,6 +96,31 @@ export const Sidebar = () => {
             </div>
           </div>
         ))}
+
+        <div className="mt-auto flex items-center justify-between">
+          <div className="flex items-center gap-2">
+            <Avatar>
+              <AvatarImage
+                src="https://api.multiavatar.com/Ranie"
+                alt="avatar"
+              />
+              <AvatarFallback>R</AvatarFallback>
+            </Avatar>
+            {!isCollapseMenu && (
+              <div>
+                <p>Ranie Kim</p>
+                <p className="text-sm font-normal text-gray-vc-600">
+                  raniekim@gmail.com
+                </p>
+              </div>
+            )}
+          </div>
+          {!isCollapseMenu && (
+            <div className="cursor-pointer">
+              <IconLogout />
+            </div>
+          )}
+        </div>
       </div>
     </div>
   )
