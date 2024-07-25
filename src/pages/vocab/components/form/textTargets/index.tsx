c366e462c75840f33f2d860792de8ced318b1d5c
ngocxxu <ngocquach43@gmail.com>
2024-07-25 20:01:01 +0700

    feat: add memo

--- src/pages/vocab/components/form/textTargets/index.tsx
+++ src/pages/vocab/components/form/textTargets/index.tsx
@@ -1,7 +1,7 @@
 import Button from '@/components/button'
 import { cn } from '@/lib/utils'
 import { IconPlus, IconX } from '@tabler/icons-react'
-import { useEffect } from 'react'
+import { memo, useEffect } from 'react'
 import {
   Control,
   Controller,
@@ -30,153 +30,155 @@ type TTextTargetsForm = {
   reset: UseFormReset<TFormInputsVocab>
 }
 
-export const TextTargetsForm = ({
-  isEditing,
-  index,
-  control,
-  errors,
-  reset,
-  setValue,
-  fieldsLengthItem
-}: TTextTargetsForm) => {
-  const { itemVocab } = useSelector((state: RootState) => state.vocabReducer)
-  const { fields, append, remove } = useFieldArray({
+export const TextTargetsForm = memo(
+  ({
+    isEditing,
+    index,
     control,
-    name: `textTarget.${index}.examples`
-  })
-  const checkErrors = Object.keys(errors).length > 0
+    errors,
+    reset,
+    setValue,
+    fieldsLengthItem
+  }: TTextTargetsForm) => {
+    const { itemVocab } = useSelector((state: RootState) => state.vocabReducer)
+    const { fields, append, remove } = useFieldArray({
+      control,
+      name: `textTarget.${index}.examples`
+    })
+    const checkErrors = Object.keys(errors).length > 0
 
-  //Editing
-  useEffect(() => {
-    if (
-      itemVocab &&
-      isEditing &&
-      // Set intial value from backend when re-render times
-      fieldsLengthItem === itemVocab.textTarget.length
-    ) {
-      reset((prev) => ({ ...prev, ...itemVocab }))
-      setValue(
-        `textTarget.${index}.wordType`,
-        itemVocab.textTarget[index].wordType
-      )
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isEditing, itemVocab])
+    //Editing
+    useEffect(() => {
+      if (
+        itemVocab &&
+        isEditing &&
+        // Set intial value from backend when re-render times
+        fieldsLengthItem === itemVocab.textTarget.length
+      ) {
+        reset((prev) => ({ ...prev, ...itemVocab }))
+        setValue(
+          `textTarget.${index}.wordType`,
+          itemVocab.textTarget[index].wordType
+        )
+      }
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [isEditing, itemVocab])
 
-  return (
-    <>
-      <div className="flex items-center justify-center gap-2">
-        <Controller
-          name={`textTarget.${index}.text`}
-          control={control}
-          rules={{ required: true }}
-          render={({ field }) => (
-            <Input
-              error={checkErrors ? errors.textTarget![index]?.text : null}
-              isMark={true}
-              label="Text target"
-              placeholder="Type here"
-              {...field}
-            />
-          )}
-        />
-        <Controller
-          name={`textTarget.${index}.wordType`}
-          control={control}
-          rules={{ required: true }}
-          render={({ field }) => (
-            <Select
-              error={errors.targetLanguage}
-              isMark={true}
-              label="Word type"
-              options={wordTypeList}
-              onChange={field.onChange}
-              value={field.value}
-            />
-          )}
-        />
-      </div>
-      <div className="flex items-center justify-center gap-2">
-        <Controller
-          name={`textTarget.${index}.explanationSource`}
-          control={control}
-          render={({ field }) => (
-            <Input
-              label="Explanation source"
-              placeholder="Type here"
-              {...field}
-            />
-          )}
-        />
-        <Controller
-          name={`textTarget.${index}.explanationTarget`}
-          control={control}
-          render={({ field }) => (
-            <Input
-              label="Explanation target"
-              placeholder="Type here"
-              {...field}
-            />
-          )}
-        />
-      </div>
-      <Controller
-        name={`textTarget.${index}.grammar`}
-        control={control}
-        render={({ field }) => (
-          <Input label="Grammar" placeholder="Type here" {...field} />
-        )}
-      />
-      <Controller
-        name={`textTarget.${index}.subject`}
-        rules={{ required: true }}
-        control={control}
-        render={({ field }) => (
-          <MultiSelect
-            error={
-              checkErrors ?
-                (errors.textTarget![index]?.subject as FieldError)
-              : null
-            }
-            isMark={true}
-            label="Subject"
-            options={subjectList}
-            {...field}
-          />
-        )}
-      />
-
-      <div className="mt-4 rounded-md border border-gray-200 p-2">
-        {fields.map((field, idx) => (
-          <fieldset key={field.id}>
-            <div
-              className={cn(
-                idx !== 0 && 'mt-4',
-                'flex items-center justify-between'
-              )}
-            >
-              <div className="text-sm">Example {idx + 1}</div>
-              <IconX
-                onClick={() => remove(idx)}
-                className="btn btn-square btn-xs btn-outline border-white bg-primary-foreground"
+    return (
+      <>
+        <div className="flex items-center justify-center gap-2">
+          <Controller
+            name={`textTarget.${index}.text`}
+            control={control}
+            rules={{ required: true }}
+            render={({ field }) => (
+              <Input
+                error={checkErrors ? errors.textTarget![index]?.text : null}
+                isMark={true}
+                label="Text target"
+                placeholder="Type here"
+                {...field}
               />
-            </div>
-            <ExamplesForm control={control} idx={idx} idxTextTarget={index} />
-          </fieldset>
-        ))}
-        <Button
-          type="button"
-          classNames={cn(fields.length !== 0 && 'mt-2', 'w-full')}
-          onClick={() => {
-            append({
-              source: '',
-              target: ''
-            })
-          }}
-          title="Example"
-          leftIcon={<IconPlus className="mr-1" />}
+            )}
+          />
+          <Controller
+            name={`textTarget.${index}.wordType`}
+            control={control}
+            rules={{ required: true }}
+            render={({ field }) => (
+              <Select
+                error={errors.targetLanguage}
+                isMark={true}
+                label="Word type"
+                options={wordTypeList}
+                onChange={field.onChange}
+                value={field.value}
+              />
+            )}
+          />
+        </div>
+        <div className="flex items-center justify-center gap-2">
+          <Controller
+            name={`textTarget.${index}.explanationSource`}
+            control={control}
+            render={({ field }) => (
+              <Input
+                label="Explanation source"
+                placeholder="Type here"
+                {...field}
+              />
+            )}
+          />
+          <Controller
+            name={`textTarget.${index}.explanationTarget`}
+            control={control}
+            render={({ field }) => (
+              <Input
+                label="Explanation target"
+                placeholder="Type here"
+                {...field}
+              />
+            )}
+          />
+        </div>
+        <Controller
+          name={`textTarget.${index}.grammar`}
+          control={control}
+          render={({ field }) => (
+            <Input label="Grammar" placeholder="Type here" {...field} />
+          )}
         />
-      </div>
-    </>
-  )
-}
+        <Controller
+          name={`textTarget.${index}.subject`}
+          rules={{ required: true }}
+          control={control}
+          render={({ field }) => (
+            <MultiSelect
+              error={
+                checkErrors ?
+                  (errors.textTarget![index]?.subject as FieldError)
+                : null
+              }
+              isMark={true}
+              label="Subject"
+              options={subjectList}
+              {...field}
+            />
+          )}
+        />
+
+        <div className="mt-4 rounded-md border border-gray-200 p-2">
+          {fields.map((field, idx) => (
+            <fieldset key={field.id}>
+              <div
+                className={cn(
+                  idx !== 0 && 'mt-4',
+                  'flex items-center justify-between'
+                )}
+              >
+                <div className="text-sm">Example {idx + 1}</div>
+                <IconX
+                  onClick={() => remove(idx)}
+                  className="btn btn-square btn-xs btn-outline border-white bg-primary-foreground"
+                />
+              </div>
+              <ExamplesForm control={control} idx={idx} idxTextTarget={index} />
+            </fieldset>
+          ))}
+          <Button
+            type="button"
+            classNames={cn(fields.length !== 0 && 'mt-2', 'w-full')}
+            onClick={() => {
+              append({
+                source: '',
+                target: ''
+              })
+            }}
+            title="Example"
+            leftIcon={<IconPlus className="mr-1" />}
+          />
+        </div>
+      </>
+    )
+  }
+)
