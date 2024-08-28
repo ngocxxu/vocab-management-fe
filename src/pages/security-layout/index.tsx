42aababd864e3cdb8ecf9047fd9536b16da9f116
NgocQuach <ngocquach43@gmail.com>
2024-08-28 22:51:47 +0700

    commit

--- src/pages/security-layout/index.tsx
+++ src/pages/security-layout/index.tsx
@@ -1,12 +1,21 @@
 import { Outlet } from 'react-router-dom'
+import LoginLeft from '../../assets/img/login-left.jpg'
 
 const SecurityLayout = () => {
-  return <div className='grid grid-cols-12'>
-    <div className='col-span-6'>A</div>
-    <div className='col-span-6'>
-      <Outlet/>
+  return (
+    <div className="lg:flex h-screen w-screen items-center justify-center">
+      <div className="max-h-[858px]">
+        <img
+          className="h-full w-full transform object-contain"
+          src={LoginLeft}
+          alt="LoginLeft"
+        />
+      </div>
+      <div className="p-28 flex flex-col items-center justify-center w-1/2">
+        <Outlet />
+      </div>
     </div>
-  </div>
+  )
 }
 
 export default SecurityLayout
--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -1,5 +1,42 @@
+import { Controller, useForm } from 'react-hook-form'
+import Logo from '../../assets/img/logo.jpg'
+import { InputLib } from '@/components/ui/input'
+
+type TFormSignin = {
+  email: string
+  password: string
+}
+
 const Signin = () => {
-  return <div>Signin</div>
+  const { handleSubmit, control } = useForm<TFormSignin>({
+    defaultValues: {
+      email: '',
+      password: ''
+    }
+  })
+  const onSubmit = () => {}
+
+  return (
+    <>
+      <img className="transform object-contain" src={Logo} alt="Logo" />
+      <h1 className="text-6xl font-bold">Welcome Back</h1>
+      <p className="text-xl text-gray-vc-400">Please login to your account</p>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          name="email"
+          control={control}
+          render={({ field }) => (
+            <InputLib
+              type='email'
+              placeholder='Email address'
+              {...field}
+            />
+          )}
+        />
+      </form>
+    </>
+  )
 }
 
 export default Signin
