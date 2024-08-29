c479abc4bff22079307e6a5db8404c888b9bc20e
bono43 <bono43@nexondv.com>
2024-08-29 16:48:11 +0700

    feat: add ui login

--- src/pages/security-layout/index.tsx
+++ src/pages/security-layout/index.tsx
@@ -3,15 +3,15 @@ import LoginLeft from '../../assets/img/login-left.jpg'
 
 const SecurityLayout = () => {
   return (
-    <div className="lg:flex h-screen w-screen items-center justify-center">
-      <div className="max-h-[858px]">
+    <div className="h-screen w-screen items-center justify-center gap-28 lg:flex">
+      <div className="max-h-[858px] max-w-[614px]">
         <img
           className="h-full w-full transform object-contain"
           src={LoginLeft}
           alt="LoginLeft"
         />
       </div>
-      <div className="p-28 flex flex-col items-center justify-center w-1/2">
+      <div className="flex max-w-[496px] flex-col items-center justify-center">
         <Outlet />
       </div>
     </div>
--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -1,6 +1,11 @@
+import IconFacebook from '@/assets/svg/IconFacebook'
+import IconGoogle from '@/assets/svg/IconGoogle'
+import Button from '@/components/button'
+import { InputLib } from '@/components/ui/input'
+import { PasswordInput } from '@/components/ui/password-input'
+import { Separator } from '@/components/ui/separator'
 import { Controller, useForm } from 'react-hook-form'
 import Logo from '../../assets/img/logo.jpg'
-import { InputLib } from '@/components/ui/input'
 
 type TFormSignin = {
   email: string
@@ -14,13 +19,22 @@ const Signin = () => {
       password: ''
     }
   })
-  const onSubmit = () => {}
+  const onSubmit = (formData: TFormSignin) => {
+    console.log(formData)
+  }
 
   return (
-    <>
-      <img className="transform object-contain" src={Logo} alt="Logo" />
-      <h1 className="text-6xl font-bold">Welcome Back</h1>
-      <p className="text-xl text-gray-vc-400">Please login to your account</p>
+    <div className="flex w-full flex-col gap-7">
+      <div className="flex justify-center">
+        <img className="transform object-contain" src={Logo} alt="Logo" />
+      </div>
+
+      <div>
+        <h1 className="mb-2 text-6xl font-bold">Welcome Back</h1>
+        <p className="text-center text-xl text-gray-vc-400">
+          Please login to your account
+        </p>
+      </div>
 
       <form onSubmit={handleSubmit(onSubmit)}>
         <Controller
@@ -28,14 +42,65 @@ const Signin = () => {
           control={control}
           render={({ field }) => (
             <InputLib
-              type='email'
-              placeholder='Email address'
+              className="mb-4 border-0"
+              type="email"
+              placeholder="Email address"
               {...field}
             />
           )}
         />
+
+        <Controller
+          name="password"
+          control={control}
+          render={({ field }) => (
+            <PasswordInput
+              placeholder="Password"
+              className="mt-4 border-0"
+              {...field}
+            />
+          )}
+        />
+
+        <Button
+          className="flex w-full justify-end p-0 underline"
+          variant="link"
+          title="Forgot password?"
+        />
+
+        <Button className="mt-6 w-full" title="Login" />
       </form>
-    </>
+
+      <div className="flex items-center justify-center gap-2">
+        <Separator className="w-20" />
+        <p className="whitespace-nowrap text-sm font-semibold">Or login with</p>
+        <Separator className="w-20" />
+      </div>
+
+      <div className="flex justify-center gap-4">
+        <Button
+          classNameTitle="ml-2"
+          className="bg-white"
+          variant="outline"
+          title="Google"
+          leftIcon={<IconGoogle />}
+        />
+        <Button
+          classNameTitle="ml-2"
+          className="bg-white"
+          variant="outline"
+          title="Facebook"
+          leftIcon={<IconFacebook />}
+        />
+      </div>
+
+      <p className="whitespace-nowrap text-center text-sm font-semibold">
+        Don't have an account?{' '}
+        <span className="cursor-pointer text-primary underline">
+          Sign up here
+        </span>
+      </p>
+    </div>
   )
 }
 
