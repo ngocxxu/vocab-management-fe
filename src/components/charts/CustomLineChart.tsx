9364a14e6be7fcc1635676eb8a9b81bcfeaab01b
bono-ste <phungbaongoc.quach@stengg.com>
2025-06-14 23:51:51 +0700

    feat: adjust recharts

--- src/components/charts/CustomLineChart.tsx
+++ src/components/charts/CustomLineChart.tsx
@@ -1,33 +1,70 @@
-import { dataLine } from '@/pages/dashboard/constants'
 import {
+  Bar,
+  BarChart,
   CartesianGrid,
-  Line,
-  LineChart,
   ResponsiveContainer,
   Tooltip,
   XAxis,
   YAxis
 } from 'recharts'
 
-const CustomLineChart = () => {
+const data = [
+  { month: 'Jan', blue: 70, yellow: 53 },
+  { month: 'Feb', blue: 53, yellow: 53 },
+  { month: 'Mar', blue: 90, yellow: 53 },
+  { month: 'Apr', blue: 38, yellow: 38 },
+  { month: 'May', blue: 53, yellow: 70 },
+  { month: 'Jun', blue: 53, yellow: 100 },
+  { month: 'Jul', blue: 85, yellow: 85 },
+  { month: 'Aug', blue: 25, yellow: 53 },
+  { month: 'Sep', blue: 73, yellow: 53 },
+  { month: 'Oct', blue: 53, yellow: 53 },
+  { month: 'Nov', blue: 88, yellow: 53 },
+  { month: 'Dec', blue: 55, yellow: 73 }
+]
+
+const ChartComponent = () => {
   return (
     <ResponsiveContainer width="100%" height="100%">
-      <LineChart width={500} height={300} data={dataLine}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-
-        <Line
-          type="linear"
-          dataKey="uv"
-          stroke="hsl(var(--primary))"
-          dot={{ r: 5 }}
-          activeDot={{ r: 8 }}
+      <BarChart data={data} barCategoryGap="20%">
+        <CartesianGrid
+          strokeDasharray="5 5"
+          stroke="#e0e0e0"
+          vertical={false}
         />
-      </LineChart>
+        <XAxis
+          dataKey="month"
+          axisLine={false}
+          tickLine={false}
+          tick={{ fontSize: 12, fill: '#666' }}
+        />
+        <YAxis
+          domain={[0, 100]}
+          axisLine={false}
+          tickLine={false}
+          tick={{ fontSize: 12, fill: '#666' }}
+        />
+        <Tooltip
+          contentStyle={{
+            backgroundColor: '#333',
+            border: 'none',
+            borderRadius: '4px',
+            color: 'white'
+          }}
+        />
+        <Bar
+          className="fill-primary-vc-500"
+          dataKey="blue"
+          radius={[5, 5, 0, 0]}
+        />
+        <Bar
+          dataKey="yellow"
+          className="fill-warning-vc-400"
+          radius={[5, 5, 0, 0]}
+        />
+      </BarChart>
     </ResponsiveContainer>
   )
 }
 
-export default CustomLineChart
+export default ChartComponent
